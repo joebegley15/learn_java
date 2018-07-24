b2a955a6898c9127ad644246e5369fe12271b35d
Joe Begley <joebegley15@gmail.com>
2018-07-24 12:37:57 -0400

    More basics

--- intro.java
+++ intro.java
@@ -15,3 +15,12 @@ public class Variables {
 
 	}
 }
+
+public class Precedence {
+	public static void main(String[] args) {
+
+		boolean riddle = !( 1 < 8 || (5 > 2 && 3 < 5));
+		System.out.println(riddle);
+
+	}
+}
