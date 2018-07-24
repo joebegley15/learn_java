5ae4e4cd5c51aadd6e721c8511d10bd61fd86d50
Joe Begley <joebegley15@gmail.com>
2018-07-24 11:30:18 -0400

    Basics

---
+++ intro.java
@@ -0,0 +1,17 @@
+public class DataTypesB {
+	public static void main(String[] args) {
+
+		System.out.println(true);
+
+	}
+}
+
+public class Variables {
+	public static void main(String[] args) {
+
+		int myNumber = 42;
+		boolean isFun = true; 
+		char movieRating = 'A'; 
+
+	}
+}
