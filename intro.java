22c0b220a38672d0b2ddd2064773797f55ee5074
Joe Begley <joebegley15@gmail.com>
2018-07-26 13:07:37 -0400

    Learning about methods

--- intro.java
+++ intro.java
@@ -91,10 +91,18 @@ public class GeneralizationsB {
 
 class Dog {
 
-
+  int age;
+	public Dog(int dogsAge) {
+  	age = dogsAge;
+  }
+  
+  public void bark() {
+    System.out.println("Woof!");
+  }
 
 
 	public static void main(String[] args) {
-
+		Dog spike = new Dog(7);
 	}
+
 }
