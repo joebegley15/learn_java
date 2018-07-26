c027f42a502136e9938ae21cb9623d7271c97861
Joe Begley <joebegley15@gmail.com>
2018-07-26 13:40:22 -0400

    Methods

--- intro.java
+++ intro.java
@@ -99,10 +99,22 @@ class Dog {
   public void bark() {
     System.out.println("Woof!");
   }
+  
+  public void run(int feet) {
+    System.out.println("Your dog ran " + feet + " feet!");
+  }
+  
+  public int getAge(){
+    return age;
+  }
 
 
 	public static void main(String[] args) {
 		Dog spike = new Dog(7);
+	    spike.bark();
+	    spike.run(1);
+	    int spikeAge = spike.getAge();
+	    System.out.println(spikeAge);
 	}
 
 }
